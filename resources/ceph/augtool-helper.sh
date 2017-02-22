fd933b7aeb0ef25c89c4b873a124fa7a64928b4b
Mark McKenna <m4rkmckenna@gmail.com>
2017-02-22 19:57:04 +0000

    Adds initial Ceph Monitor [Node,Cluster]

---
+++ resources/ceph/augtool-helper.sh
@@ -0,0 +1,11 @@
+--2017-02-22 11:28:28--  https://raw.githubusercontent.com/brooklyncentral/brooklyn-rethinkdb/master/resources/rethinkdb/augtool-helper.sh
+Resolving raw.githubusercontent.com... 151.101.16.133
+Connecting to raw.githubusercontent.com|151.101.16.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 860 [text/plain]
+Saving to: ‘augtool-helper.sh’
+
+     0K                                                       100% 51.3M=0s
+
+2017-02-22 11:28:28 (51.3 MB/s) - ‘augtool-helper.sh’ saved [860/860]
+
